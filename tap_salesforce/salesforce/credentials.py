6a1c1bd8778cd5388f48a5838438e18e06f668a8
Cameron Davison <cameron@steadily.com>
2025-02-06 11:38:43 -0600

    add request timeout

--- tap_salesforce/salesforce/credentials.py
+++ tap_salesforce/salesforce/credentials.py
@@ -94,6 +94,7 @@ class SalesforceAuthOAuth(SalesforceAuth):
                 self._login_url,
                 data=self._login_body,
                 headers={"Content-Type": "application/x-www-form-urlencoded"},
+                timeout=30,
             )
 
             resp.raise_for_status()
